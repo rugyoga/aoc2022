1e9e5e605c002222f43f5b744c8eb945fa2d84aa
Guy Argo <guy.argo@gmail.com>
2022-12-04 22:03:44 -0800

    day5

---
+++ day05.exs
@@ -0,0 +1,43 @@
+defmodule Stack do
+    def transfer([amount, from, to], stacks) do
+        IO.inspect(stacks, label: "stacks")
+        IO.inspect(amount, label: "amount")
+        IO.inspect(from, label: "from")
+        IO.inspect(to, label: "to")
+        if amount == 0 do
+            stacks
+        else
+            [x | from_new] = Enum.at(stacks, from-1)
+            to_new = [x | Enum.at(stacks, to-1)]
+            transfer([amount-1, from, to], stacks |> List.replace_at(from-1, from_new) |> List.replace_at(to-1, to_new))
+        end
+    end
+end
+
+[stacks, moves] = "day05.txt" |> File.read! |> String.split("\n\n", trim: true) |> Enum.map(&String.split(&1,"\n", trim: true))
+
+stacks =
+stacks |> 
+Enum.map(
+    fn s -> 
+        xs = String.split(s, "", trim: true) 
+        Enum.map([1,5,9,13,17,21,25,29,33], &Enum.at(xs, &1))
+    end) 
+|> Enum.zip_with(&(&1))
+|> Enum.map(fn l -> Enum.drop_while(l, &(&1 == " ")) end)
+|> IO.inspect
+
+
+
+moves 
+|> Enum.map(
+    fn s -> 
+        xs = String.split(s, " ", trim: true)
+        Enum.map([1,3,5], &Enum.at(xs, &1) |> String.to_integer)
+    end)
+|> Enum.reduce(stacks, &Stack.transfer/2)
+|> Enum.map(&List.first/1)
+|> Enum.join("")
+|> IO.puts
+
+
