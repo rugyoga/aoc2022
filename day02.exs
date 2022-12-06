7c97190709f09fd00a1912d4dcbf2010d8a3a61e
Guy Argo <guy.argo@gmail.com>
2022-12-06 01:19:25 -0800

    day 2 tidy up

--- day02.exs
+++ day02.exs
@@ -1,3 +1,56 @@
-Code.require_file("rps.ex")
+defmodule Day02 do
+    @them %{ "A" => :rock, "B" => :paper, "C" => :scissors }
+    @us %{ "X" => :rock, "Y" => :paper, "Z" => :scissors }
+    @score %{ rock: 1, paper: 2, scissors: 3}
 
-"day02.txt" |> RPS.input |> Enum.map(&RPS.part1/1) |> Enum.sum |> IO.inspect
+    def them(x), do: @them[x]
+    def us(x), do: @us[x]
+    def score(x), do: @score[x]
+
+    def result(:rock,     :paper),    do: 6
+    def result(:rock,     :scissors), do: 0
+    def result(:rock,     :rock),     do: 3
+    def result(:paper,    :paper),    do: 3
+    def result(:paper,    :scissors), do: 6
+    def result(:paper,    :rock),     do: 0
+    def result(:scissors, :paper),    do: 0
+    def result(:scissors, :scissors), do: 3
+    def result(:scissors, :rock),     do: 6
+
+    def wanted(:rock, "X"), do: :scissors
+    def wanted(:rock, "Y"), do: :rock
+    def wanted(:rock, "Z"), do: :paper
+    def wanted(:paper, "X"), do: :rock
+    def wanted(:paper, "Y"), do: :paper
+    def wanted(:paper, "Z"), do: :scissors
+    def wanted(:scissors, "X"), do: :paper
+    def wanted(:scissors, "Y"), do: :scissors
+    def wanted(:scissors, "Z"), do: :rock
+
+    def input() do
+        "day02.txt"
+        |> File.read!
+        |> String.split("\n", trim: true)
+        |> Enum.map(&String.split(&1, " ", trim: true))
+    end
+
+    def standard([a, b]) do
+        x = them(a)
+        y = us(b)
+        result(x, y) + score(y)
+    end
+
+    def to_order([a, b]) do
+        x = them(a)
+        y = wanted(x, b)
+        result(x, y) + score(y)
+    end
+
+    def part1, do: input() |> Enum.map(&standard/1) |> Enum.sum
+    def part2, do: input() |> Enum.map(&to_order/1) |> Enum.sum
+end
+
+Day02.part1() |> IO.inspect(label: "part1")
+Day02.part2() |> IO.inspect(label: "part2")
+
+
--- day02b.exs
+++
@@ -1,3 +0,0 @@
-Code.require_file("rps.ex")
-
-"day02.txt" |> RPS.input |> Enum.map(&RPS.part2/1) |> Enum.sum |> IO.inspect
