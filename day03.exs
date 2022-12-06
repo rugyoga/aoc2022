5afba9fb1607f222558920ff9d1ac996489b82b9
Guy Argo <guy.argo@gmail.com>
2022-12-06 01:11:03 -0800

    day 3 tidy up

--- day03.exs
+++ day03.exs
@@ -1,26 +1,37 @@
-p = fn x ->
-        if x in ?a..?z do
-            (x - ?a + 1) |> IO.inspect
-        else
-            (x - ?A + 27) |> IO.inspect
-        end
+defmodule Day03 do
+    def encode(x), do: if(x in ?a..?z, do: x - ?a + 1, else: x - ?A + 27)
+
+    def input, do: "day03.txt" |> File.read! |> String.split("\n", trim: true)
+
+    def part1 do
+        input()
+        |> Enum.map(
+            fn s ->
+                l = s |> to_charlist()
+                {a, b} = Enum.split(l, div(Enum.count(l), 2))
+                MapSet.intersection(MapSet.new(a), MapSet.new(b))
+                |> Enum.at(0)
+                |> encode()
+            end
+        )
+        |> Enum.sum
     end
 
-"day03.txt"
-|> File.read!
-|> String.split("\n", trim: true)
-|> Enum.map(
-    fn s ->
-        l = String.codepoints(s)
-        n = div(Enum.count(l), 2)
-        a = Enum.take(l, n) |> MapSet.new()
-        b = Enum.drop(l, n) |> MapSet.new()
-        MapSet.intersection(a, b)
-        |> Enum.to_list
-        |> List.first
-        |> :binary.first
-        |> then(p)
+    def part2 do
+        input()
+        |> Enum.chunk_every(3)
+        |> Enum.map(
+            fn as ->
+                as 
+                |> Enum.map(&(&1 |> to_charlist() |> MapSet.new()))
+                |> Enum.reduce(&MapSet.intersection/2)
+                |> Enum.at(0)
+                |> encode()
+            end
+        )
+        |> Enum.sum
     end
-)
-|> Enum.sum
-|> IO.inspect
+end
+        
+Day03.part1() |> IO.inspect(label: "part1")
+Day03.part2() |> IO.inspect(label: "part2")
