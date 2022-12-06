03810ba99d3d1543544bd2d11d59ed62682e5446
Guy Argo <guy.argo@gmail.com>
2022-12-06 01:24:16 -0800

    day 1 tidy up

--- day01.exs
+++ day01.exs
@@ -1,6 +1,14 @@
-"day01.txt"
-|> File.read!
-|> String.split("\n\n", trim: true)
-|> Enum.map(fn s -> s |> String.split("\n", trim: true) |> Enum.map(&String.to_integer/1) |> Enum.sum() end)
-|> Enum.max
-|> IO.inspect
+defmodule Day01 do
+    def input do
+        "day01.txt"
+        |> File.read!
+        |> String.split("\n\n", trim: true)
+        |> Enum.map(fn s -> s |> String.split("\n", trim: true) |> Enum.map(&String.to_integer/1) |> Enum.sum() end)
+    end
+
+    def part1, do: input() |> Enum.max
+    def part2, do: input() |> Enum.sort(:desc) |> Enum.take(3) |> Enum.sum
+end
+
+Day01.part1() |> IO.inspect(label: "part1")
+Day01.part2() |> IO.inspect(label: "part2")
--- day01b.exs
+++
@@ -1,9 +0,0 @@
-"day01.txt"
-|> File.read!
-|> String.split("\n\n", trim: true)
-|> Enum.map(fn s -> s |> String.split("\n", trim: true) |> Enum.map(&String.to_integer/1) |> Enum.sum() end)
-|> Enum.sort
-|> Enum.reverse
-|> Enum.take(3)
-|> Enum.sum
-|> IO.inspect
