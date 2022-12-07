937578dc24077e76981f7c3e0c1d7fb3a787a445
Guy Argo <guy.argo@gmail.com>
2022-12-06 22:37:01 -0800

    day 7

---
+++ day07.exs
@@ -0,0 +1,62 @@
+defmodule Day07 do
+    def input, do: "day07.txt" |> File.read! |> String.split("\n", trim: true)
+
+    def count(n, message \\ input()) do
+        message
+        |> Enum.chunk_every(n, 1)
+        |> Enum.take_while(fn items -> (Enum.count(MapSet.new(items))) != n end)
+        |> Enum.count
+        |> then(&(&1+n))
+    end
+
+    def parse([], _path, tree), do: tree
+    def parse([line | lines], path, tree) do
+        if line == "$ ls" do
+            {listing, lines} = Enum.split_while(lines, fn line -> !String.starts_with?(line, "$") end)
+            {dirs, files} = listing |> Enum.split_with(&String.starts_with?(&1, "dir"))
+            dirs = dirs |> Enum.map(fn dir -> dir |> String.split(" ", trim: true) |> List.last() end)
+            tree = dirs |> Enum.reduce(tree, fn dir, tree -> put_in(tree, [dir | path] |> Enum.reverse(), %{}) end)
+            tree = files
+                 |> Enum.map(&String.split(&1, " ", trim: true)) 
+                 |> Enum.reduce(tree, fn [size, name], tree -> put_in(tree, [name | path] |> Enum.reverse(), String.to_integer(size)) end)
+            parse(lines, path, tree)
+        else
+            [_, _, dir] = String.split(line, " ", trim: true)
+            case dir do
+            ".." ->
+                [_hd | tail] = path
+                parse(lines, tail, tree)
+            "/" ->
+                parse(lines, [], tree)
+            _ -> 
+                parse(lines, [dir | path], tree)
+            end
+        end
+    end
+
+    def size(tree, path, size_map) do
+        {sizes, subtrees} = tree |> Enum.split_with(fn {_, v} -> is_integer(v) end)
+        subtotal = sizes |> Enum.unzip() |> elem(1) |> Enum.sum()
+        Enum.reduce(
+            subtrees,
+            {subtotal, size_map}, 
+            fn {key, subtree}, {subtotal, size_map} ->
+                path_new = [key | path]
+                {subtree_size, size_map} = size(subtree, path_new, size_map)
+                {subtotal + subtree_size, Map.put(size_map, Enum.join(path_new, "/"), subtree_size)}
+            end)
+    end
+        
+
+
+    def part1, do: parse(input(), [], %{}) |> size(["/"], %{}) |> elem(1) |> Map.values() |> Enum.filter(&(&1 <= 100_000)) |> Enum.sum
+    def part2 do
+        {total, sizes} = parse(input(), [], %{}) |> size(["/"], %{})
+        free_space = 70_000_000 - total
+        needed = 30_000_000 - free_space
+        sizes |> Map.values |> Enum.sort |> Enum.drop_while(&(&1 < needed)) |> List.first()
+    end
+end
+
+Day07.part1() |> IO.inspect(label: "part1")
+Day07.part2() |> IO.inspect(label: "part2")
