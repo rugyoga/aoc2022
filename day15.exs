3186cf90ecd6122e8a1d1082a82582f32d8b4ade
Guy Argo <guy.argo@gmail.com>
2022-12-28 16:15:17 -0800

    Day 15 refactor

--- day15.exs
+++ day15.exs
@@ -2,22 +2,6 @@ defmodule Day15 do
     def input do
         "day15.txt"
         |> File.read!
-# """
-# Sensor at x=2, y=18: closest beacon is at x=-2, y=15
-# Sensor at x=9, y=16: closest beacon is at x=10, y=16
-# Sensor at x=13, y=2: closest beacon is at x=15, y=3
-# Sensor at x=12, y=14: closest beacon is at x=10, y=16
-# Sensor at x=10, y=20: closest beacon is at x=10, y=16
-# Sensor at x=14, y=17: closest beacon is at x=10, y=16
-# Sensor at x=8, y=7: closest beacon is at x=2, y=10
-# Sensor at x=2, y=0: closest beacon is at x=2, y=10
-# Sensor at x=0, y=11: closest beacon is at x=2, y=10
-# Sensor at x=20, y=14: closest beacon is at x=25, y=17
-# Sensor at x=17, y=20: closest beacon is at x=21, y=22
-# Sensor at x=16, y=7: closest beacon is at x=15, y=3
-# Sensor at x=14, y=3: closest beacon is at x=15, y=3
-# Sensor at x=20, y=1: closest beacon is at x=15, y=3
-# """
         |> String.split("\n", trim: true)
         |> Enum.reduce({[], MapSet.new}, &extract/2)
         |> then(fn {sensors, beacon_set} -> %{sensors: sensors, beacons: beacons_on_y(beacon_set)} end)
